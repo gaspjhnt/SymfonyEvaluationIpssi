0641771c5a71a676ea634feb691d613d7f927e8d
Océane Princep <94532496+oce-prcp@users.noreply.github.com>
2023-12-07 11:21:28 +0100

    [In Progress] add comments controller

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -13,24 +13,28 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+// This RegistrationFormType allows users to enter their email address, surname, first name and password, 
+// and to accept the terms and conditions for registration. Validation constraints are applied to guarantee the quality of the data entered.
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Create a form field for each property of the User entity.
         $builder
             ->add('email', EmailType::class)
             ->add('nom')
             ->add('prenom')
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
                 'constraints' => [
+                    // Required to accept the terms and conditions.
                     new IsTrue([
                         'message' => 'Vous devez accpeter nos terms.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -51,6 +55,7 @@ class RegistrationFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // set default data class for the form
         $resolver->setDefaults([
             'data_class' => User::class,
         ]);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,23 +11,28 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+// This UserType form lets you modify a user's basic data, such as email address, surname and first name. 
+// If the user is an administrator (ROLE_ADMIN), the form also lets you change the user's roles between administrator and standard user.
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // We add the fields we want to display in the form.
         $builder
             ->add('email')
             ->add('Nom')
             ->add('Prenom');
 
-        $user = $options['data']; // Récupérer l'utilisateur transmis au formulaire
+        $user = $options['data']; // We retrieve the user object from the form options.
 
+        // If the user is an administrator, we add the roles field.
         if (in_array('ROLE_ADMIN', $user->getRoles())) {
             $builder->add('roles', ChoiceType::class, [
                 'choices' => [
                     'Admin' => 'ROLE_ADMIN',
                     'Utilisateur' => 'ROLE_USER',
                 ],
+                // We display the roles as checkboxes.
                 'multiple' => true,
                 'expanded' => true,
             ]);
@@ -36,6 +41,7 @@ class UserType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // We configure the form to use the User class.
         $resolver->setDefaults([
             'data_class' => User::class,
         ]);
